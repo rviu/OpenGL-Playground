0e3c95e1969fb2dbdc7c3b23537dfc889706d109
rviu <134678217+rviu@users.noreply.github.com>
2023-06-25 11:39:46 +0000

    Add lighting

---
+++ src/lighting.cpp
@@ -0,0 +1,229 @@
+#include <iostream>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "camera.hpp"
+#include "shader.hpp"
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double x_pos, double y_pos);
+void scroll_callback(GLFWwindow* window, double x_offset, double y_offset);
+void process_input(GLFWwindow* window);
+
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+Camera camera(glm::vec3(0.0f, 0.0f, 6.0f));
+bool first_mouse = true;
+float last_x = SCR_WIDTH / 2.0f;
+float last_y = SCR_HEIGHT / 2.0f;
+
+float delta_time = 0.0f;
+float last_frame_time = 0.0f;
+
+glm::vec3 light_pos(1.2f, 1.0f, 2.0f);
+
+int main() {
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  GLFWwindow* window =
+      glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Lighting", NULL, NULL);
+
+  if (!window) {
+    std::cout << "Failed to create GLFW window\n";
+    glfwTerminate();
+
+    return -1;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+  glfwSetCursorPosCallback(window, mouse_callback);
+  glfwSetScrollCallback(window, scroll_callback);
+
+  glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cout << "Failed to initialise GLAD\n";
+
+    return -1;
+  }
+
+  glEnable(GL_DEPTH_TEST);
+
+  Shader object_shader("src/shader/lighting_object.vs", "src/shader/lighting_object.fs");
+  Shader light_source_shader("src/shader/lighting_source.vs", "src/shader/lighting_source.fs");
+
+  float vertices[] = {
+    -0.5f, -0.5f, -0.5f,
+     0.5f, -0.5f, -0.5f,
+     0.5f,  0.5f, -0.5f,
+     0.5f,  0.5f, -0.5f,
+    -0.5f,  0.5f, -0.5f,
+    -0.5f, -0.5f, -0.5f,
+
+    -0.5f, -0.5f,  0.5f,
+     0.5f, -0.5f,  0.5f,
+     0.5f,  0.5f,  0.5f,
+     0.5f,  0.5f,  0.5f,
+    -0.5f,  0.5f,  0.5f,
+    -0.5f, -0.5f,  0.5f,
+
+    -0.5f,  0.5f,  0.5f,
+    -0.5f,  0.5f, -0.5f,
+    -0.5f, -0.5f, -0.5f,
+    -0.5f, -0.5f, -0.5f,
+    -0.5f, -0.5f,  0.5f,
+    -0.5f,  0.5f,  0.5f,
+
+     0.5f,  0.5f,  0.5f,
+     0.5f,  0.5f, -0.5f,
+     0.5f, -0.5f, -0.5f,
+     0.5f, -0.5f, -0.5f,
+     0.5f, -0.5f,  0.5f,
+     0.5f,  0.5f,  0.5f,
+
+    -0.5f, -0.5f, -0.5f,
+     0.5f, -0.5f, -0.5f,
+     0.5f, -0.5f,  0.5f,
+     0.5f, -0.5f,  0.5f,
+    -0.5f, -0.5f,  0.5f,
+    -0.5f, -0.5f, -0.5f,
+
+    -0.5f,  0.5f, -0.5f,
+     0.5f,  0.5f, -0.5f,
+     0.5f,  0.5f,  0.5f,
+     0.5f,  0.5f,  0.5f,
+    -0.5f,  0.5f,  0.5f,
+    -0.5f,  0.5f, -0.5f
+  };
+
+  unsigned int VBO, object_VAO;
+  glGenVertexArrays(1, &object_VAO);
+  glGenBuffers(1, &VBO);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glBindVertexArray(object_VAO);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+
+  unsigned int light_source_VAO;
+  glGenVertexArrays(1, &light_source_VAO);
+  glBindVertexArray(light_source_VAO);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+
+  while (!glfwWindowShouldClose(window)) {
+    float current_frame_time = static_cast<float>(glfwGetTime());
+    delta_time = current_frame_time - last_frame_time;
+    last_frame_time = current_frame_time;
+
+    process_input(window);
+
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    object_shader.use();
+    object_shader.set_uniform_vec3("object_color", 1.0f, 0.5f, 0.31f);
+    object_shader.set_uniform_vec3("light_color", 1.0f, 1.0f, 1.0f);
+
+    glm::mat4 projection = glm::perspective(glm::radians(camera.zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+    glm::mat4 view = camera.get_view_matrix();
+    object_shader.set_uniform_mat4("projection", projection);
+    object_shader.set_uniform_mat4("view", view);
+
+    glm::mat4 model = glm::mat4(1.0f);
+    object_shader.set_uniform_mat4("model", model);
+
+    glBindVertexArray(object_VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 36);
+
+    light_source_shader.use();
+    light_source_shader.set_uniform_mat4("projection", projection);
+    light_source_shader.set_uniform_mat4("view", view);
+    model = glm::mat4(1.0f);
+    model = glm::translate(model, light_pos);
+    model = glm::scale(model, glm::vec3(0.2f));
+    light_source_shader.set_uniform_mat4("model", model);
+
+    glBindVertexArray(light_source_VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 36);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glDeleteVertexArrays(1, &object_VAO);
+  glDeleteVertexArrays(1, &light_source_VAO);
+  glDeleteBuffers(1, &VBO);
+  object_shader.delete_program();
+  light_source_shader.delete_program();
+
+  glfwTerminate();
+
+  return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+  glfwSetWindowAspectRatio(window, SCR_WIDTH, SCR_HEIGHT);
+  glViewport(0, 0, width, height);
+}
+
+void mouse_callback(GLFWwindow* window, double x_pos_in, double y_pos_in) {
+  float x_pos = static_cast<float>(x_pos_in);
+  float y_pos = static_cast<float>(y_pos_in);
+
+  if (first_mouse) {
+    last_x = x_pos;
+    last_y = y_pos;
+    first_mouse = false;
+  }
+
+  float x_offset = x_pos - last_x;
+  float y_offset = last_y - y_pos;
+  last_x = x_pos;
+  last_y = y_pos;
+
+  camera.process_mouse_movement(x_offset, y_offset);
+}
+
+void scroll_callback(GLFWwindow* window, double x_offset, double y_offset) {
+  camera.process_mouse_scroll(static_cast<float>(y_offset));
+}
+
+void process_input(GLFWwindow* window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, true);
+  }
+
+  if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+    camera.process_keyboard(FORWARD, delta_time);
+  }
+
+  if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+    camera.process_keyboard(BACKWARD, delta_time);
+  }
+
+  if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+    camera.process_keyboard(LEFT, delta_time);
+  }
+
+  if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+    camera.process_keyboard(RIGHT, delta_time);
+  }
+}
